07a14744166df3000325b188a28f006483a4fbc5
Georg Peter Modzelewski <melitta@tzi.de>
2012-01-31 20:49:08 +0100

    hangmaaan

---
+++ Uebung1/hangMan.rb
@@ -0,0 +1,40 @@
+#hangMan.rb
+
+puts "Bitte geben Sie das Wort ein, was zu loesen ist:"
+word = gets
+word.chop!
+incorrects = 0
+maxIncorrects = 4
+result = "";
+word.size.times{result = result + "_"}
+puts "Bitte geben Sie einen Wort ein!"
+
+while (incorrects < maxIncorrects)
+ guess = gets
+ guess.chop!
+ y = 0
+ isInside = false
+ word.each_char do |pos|
+    if pos == guess
+       result[y] = pos
+       isInside = true
+    end
+    y += 1
+ end
+
+ if !isInside
+   incorrects = incorrects+1
+ end
+
+ if result == word
+   puts "Sie haben gewonnen!"
+   exit
+ end
+
+ puts result
+ print  "Sie haben noch ",maxIncorrects-incorrects," Versuche"
+end
+
+puts "You loose!!!"
+exit
+
