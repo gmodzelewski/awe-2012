742558f33174c2922bb9bdf88bcc080c1b05c29c
Georg Peter Modzelewski <melitta@tzi.de>
2012-02-05 18:18:21 +0100

    problem mit heroku

--- rails_projects/first_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '4f60e7aad8097276fa71276467589e7988e8fca411600073cd9fd150e262c2c559090bca963b44fa907905fe9f222a307c1d76bcc7b914816478b66540542c33'
