75d705e7c515cb4ca7af8d2b0e8adbbcd253ca30
mareike <mareikep@tzi.de>
2012-02-07 19:30:28 +0100

    aggro commit

--- Uebung2/gcd.rb
+++ Uebung2/gcd.rb
@@ -1,11 +1,14 @@
 class GCD
 
   def greatest_common_divisor(num0, num1)
-
-      if num1 == 0
-        return num0
-        elsif num0 < num1
-          greatest_common_divisor(num1, num0)
-         end
-       end      
+  if num1 == 0
+      return num0 
+    end
+     
+    if num0 < num1
+      greatest_common_divisor(num1, num0) 
+    else
+      greatest_common_divisor(num1, num0 % num1)
+    end
   end
+end
--- Uebung2/gcd_user_input.rb
+++ Uebung2/gcd_user_input.rb
@@ -2,8 +2,8 @@
 require './gcd'
 
 puts "Insert the first number"
-num0 = gets
+num0 = gets.to_i
 puts "Insert the second number"
-num1 = gets
+num1 = gets.to_i
 
 puts GCD.new.greatest_common_divisor(num0, num1)
