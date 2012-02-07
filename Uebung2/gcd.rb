d83e8e42c59c48d6ad8d185095830d0a0343a5a7
mareike <mareikep@tzi.de>
2012-02-07 18:40:35 +0100

    refactor gcd

--- Uebung2/gcd.rb
+++ Uebung2/gcd.rb
@@ -1,12 +1,11 @@
 class GCD
 
-	def greatest_common_divisor(num0, num1)
+  def greatest_common_divisor(num0, num1)
 
-    	if num1.nil
-    		puts num0
-    		return num0
-    		elsif num0 < num1
-    			greatest_common_divisor(num1, num0)
-   			end
-   		end    	
-	end
+      if num1 == 0
+        return num0
+        elsif num0 < num1
+          greatest_common_divisor(num1, num0)
+         end
+       end      
+  end
