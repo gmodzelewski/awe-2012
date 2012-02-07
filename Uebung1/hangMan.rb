af0623188109f24925b1c53123a52c2e5fcd051d
mareike <mareikep@tzi.de>
2012-02-07 17:15:05 +0100

    refactoring in progress... dumddiiidiidiiidumm... frzl

--- Uebung1/hangMan.rb
+++ Uebung1/hangMan.rb
@@ -7,26 +7,29 @@ result = ""
 puts "Willkommen bei Hangman. Sie haben insgesamt #{max_incorrects} Versuche zur Verfuegung."
 
 word = File.open('/usr/share/dict/words').readlines.sample.chomp.upcase
-puts word
+word.size.times { result += "_" } 
 
-puts "Bitte geben Sie einen Buchstaben ein. \n"
+puts result
+
+
+puts word
+puts "Bitte geben Sie einen Buchstaben ein."
 
 while (incorrects < max_incorrects)
- guess = gets
- guess.chomp!
- guess.upcase!
+ guess = gets.chomp.upcase
+ 
  y = 0
  is_inside = false
- word.each_char do |pos|
-    if pos == guess
-       result[y] = pos
+ word.each_char do |letter|
+    if letter == guess
+       result[y] = letter
        is_inside = true
     end
     y += 1
  end
 
  if !is_inside
-   incorrects = incorrects+1
+   incorrects = incorrects + 1
  end
 
  if result == word
@@ -35,9 +38,12 @@ while (incorrects < max_incorrects)
  end
 
  puts result
- puts  "Sie haben noch ",max_incorrects-incorrects," Versuche \n"
+ remaining = max_incorrects - incorrects
+ puts remaining
+ 
+ puts  "Sie haben noch #{remaining} Versuche"
 end
 
-puts "Sie haben leider nicht gewonnen. Es tut mir leid"
+puts "Sie haben leider nicht gewonnen. Hangover!"
 exit
 
