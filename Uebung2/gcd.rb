7255890ed8871ea9b7c2f1fa1413978d9a3f385e
Georg Peter Modzelewski <melitta@tzi.de>
2012-02-07 18:30:51 +0100

    Der groesste gemeinsame Teiler

---
+++ Uebung2/gcd.rb
@@ -0,0 +1,17 @@
+class GCD
+
+num0 = ARGV[0]
+num1 = ARGV[1]
+
+	def greatest_common_divisor(num0, num1)
+    return false if !(num0.is_a? Integer)
+    return false if !(num1.is_a? Integer)
+
+    	if num1.nil
+    		puts num0
+    		return num0
+    		elsif num0 < num1
+    			greatest_common_divisor(num1, num0)
+   			end
+   		end    	
+	end
