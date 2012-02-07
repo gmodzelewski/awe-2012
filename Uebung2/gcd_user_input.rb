79a32899800ce3ceeba3e19c206e08f2f407c768
mareike <mareikep@tzi.de>
2012-02-07 20:04:28 +0100

    merkwürdiger testfehler?§

--- Uebung2/gcd_user_input.rb
+++ Uebung2/gcd_user_input.rb
@@ -1,9 +1,9 @@
 #zum manuellen Testen diese Datei aufrufen
 require './gcd'
 
-puts "Insert the first number"
-num0 = gets.to_i
-puts "Insert the second number"
-num1 = gets.to_i
+puts "Insert the first integer number"
+num0 = gets.to_i.abs
+puts "Insert the second integer number"
+num1 = gets.to_i.abs
 
 puts GCD.new.greatest_common_divisor(num0, num1)
